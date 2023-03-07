0a471e867a66c46c8c084cb7855f201b7f22f8f8
yak <its.yak@gmail.com>
2023-03-07 23:02:20 +0200

    lottie player and samples

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import lottieSamplesList from "./assets/lottie-sample-list.json";
+
+// src="https://assets3.lottiefiles.com/packages/lf20_UJNc2t.json"
 
 function App() {
+  const [lottieIdx, setLottieIdx] = useState(0);
+  const lottieItem = lottieSamplesList.samples[lottieIdx];
+  const lottieUrl = `${process.env.PUBLIC_URL}/lottie-samples/${lottieItem.file}`;
+
+  const handleSelect = (event: React.FormEvent) => {
+    let v = (event.target as any).value;
+    console.log("change lottie", v, lottieSamplesList.samples[v].name);
+    setLottieIdx(v);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>HELLO</h1>
       </header>
+      <main>
+        <select
+          name="selectLottie"
+          id="selectLottie"
+          value={lottieIdx}
+          onChange={(event) => handleSelect(event)}
+        >
+          {lottieSamplesList.samples.map((item, i) => (
+            <option key={item.name} value={i}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+        <div>{lottieUrl}</div>
+        {/* <pre>{JSON.stringify(lottieSamplesList, null, 2)}</pre> */}
+        <Player
+          autoplay
+          loop
+          src={lottieUrl}
+          style={{ height: "300px", width: "300px" }}
+        >
+          <Controls
+            visible={true}
+            buttons={["play", "repeat", "frame", "debug"]}
+          />
+        </Player>
+      </main>
     </div>
   );
 }

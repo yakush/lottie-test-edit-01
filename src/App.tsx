0963299db8355094662d25b31278cd35db9a8192
yak <its.yak@gmail.com>
2023-03-09 23:38:21 +0200

    jsons

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,49 @@ import { useLottieContext } from "./LottieContext";
 function App() {
   const lottie = useLottieContext();
 
+  function dragOverHandler(ev) {
+    console.log("File(s) in drop zone");
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  }
+  
+  function dropHandler(ev) {
+    console.log("File(s) dropped");
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      [...ev.dataTransfer.items].forEach((item, i) => {
+        // If dropped items aren't files, reject them
+        if (item.kind === "file") {
+          const file = item.getAsFile();
+          console.log(`… file[${i}].name = ${file.name}`);
+        }
+      });
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      [...ev.dataTransfer.files].forEach((file, i) => {
+        console.log(`… file[${i}].name = ${file.name}`);
+      });
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <div>LOTTIE EDITOR TEST</div>
+        <div
+          id="drop_zone"
+          onDrop={dropHandler}
+          onDragOver={dragOverHandler}
+        >
+          <p>
+            Drag one or more files to this <i>drop zone</i>.
+          </p>
+        </div>
       </header>
       <main className="App-main">
         <div className="editor-wrapper">

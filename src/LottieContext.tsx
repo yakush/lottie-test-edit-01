d7ea675d466a92a384e77c4d975992398a8e7ff1
yak <its.yak@gmail.com>
2023-03-10 00:38:37 +0200

    drag and drop json file

--- src/LottieContext.tsx
+++ src/LottieContext.tsx
@@ -9,6 +9,7 @@ export interface ILottieContext {
 
   setJson: React.Dispatch<React.SetStateAction<LottieJson>>;
   loadUrl: (url: string) => Promise<LottieJson>;
+  loadFile: (file: File) => Promise<LottieJson>;
 }
 const initialState: ILottieContext = {
   url: "",
@@ -16,6 +17,7 @@ const initialState: ILottieContext = {
   isLoading: false,
   error: undefined,
   loadUrl: () => Promise.reject<LottieJson>(),
+  loadFile: () => Promise.reject<LottieJson>(),
   setJson: () => {},
 };
 
@@ -51,6 +53,53 @@ const LottieContextProvider: React.FC<{ children: React.ReactNode }> = ({
     }
   }, []);
 
+  const loadFile = useCallback((file: File) => {
+    return new Promise<LottieJson>((resolve, reject) => {
+      setUrl("");
+      setIsLoading(true);
+      setError(undefined);
+      setJson(undefined);
+
+      const reader = new FileReader();
+
+      reader.onabort = () => {
+        console.log("file reading was aborted");
+        setIsLoading(false);
+        setError("file reading was aborted");
+        setJson(undefined);
+        reject("file reading was aborted");
+      };
+
+      reader.onerror = () => {
+        console.log("file reading has failed");
+        setIsLoading(false);
+        setError("file reading has failed");
+        setJson(undefined);
+        reject("file reading has failed");
+      };
+
+      reader.onload = () => {
+        try {
+          const str = reader.result as string;
+          const json = JSON.parse(str);
+          setJson(json);
+          setIsLoading(false);
+          setError(undefined);
+
+          console.log(json);
+          resolve(json);
+        } catch (err) {
+          console.log("JSON parsing failed");
+          setIsLoading(false);
+          setError("JSON parsing failedF");
+          setJson(undefined);
+          reject("JSON parsing failedF");
+        }
+      };
+      reader.readAsText(file);
+    });
+  }, []);
+
   return (
     <LottieContext.Provider
       value={{
@@ -60,6 +109,7 @@ const LottieContextProvider: React.FC<{ children: React.ReactNode }> = ({
         json,
         setJson,
         loadUrl,
+        loadFile,
       }}
     >
       {children}
--- src/comps/LottieFileSelector.tsx
+++ src/comps/LottieFileSelector.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import styles from "./LottieFileSelector.module.css";
+import { useDropzone } from "react-dropzone";
 
 import lottieSamplesList from "../assets/lottie-sample-list.json";
 import { useLottieContext } from "../LottieContext";
@@ -14,10 +15,28 @@ function createUrl(file: string) {
 
 const LottieFileSelector: React.FC<Props> = ({}) => {
   const lottie = useLottieContext();
-
   const [lottieIdx, setLottieIdx] = useState(0);
 
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    if (acceptedFiles && acceptedFiles.length) {
+      try {
+        const file = acceptedFiles[0];
+        await lottie.loadFile(file);
+        setLottieIdx(-1);
+      } catch (e) {
+        setLottieIdx(-1);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   useEffect(() => {
+    if (lottieIdx < 0) {
+      return;
+    }
+
     const item = lottieSamplesList.samples[lottieIdx];
     const url = createUrl(item.file);
 
@@ -34,20 +53,30 @@ const LottieFileSelector: React.FC<Props> = ({}) => {
 
   return (
     <div className={styles.root}>
-      <label htmlFor="selectLottie">select lottie</label>
-      <select
-        name="selectLottie"
-        id="selectLottie"
-        value={lottieIdx}
-        onChange={(event) => handleSelect(event)}
-      >
-        {lottieSamplesList.samples.map((item, i) => (
-          <option key={item.name} value={i}>
-            {item.name}
-          </option>
-        ))}
-      </select>
-      <div>{lottie.url}</div>
+      <div className={styles.selector}>
+        <label htmlFor="selectLottie">select lottie</label>
+        <select
+          name="selectLottie"
+          id="selectLottie"
+          value={lottieIdx}
+          onChange={(event) => handleSelect(event)}
+        >
+          {lottieSamplesList.samples.map((item, i) => (
+            <option key={item.name} value={i}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+        {/* <div>{lottie.url}</div> */}
+      </div>
+      <div className={styles.dropZone} {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <div>Drop it like it's hot</div>
+        ) : (
+          <div>Drop JSON files here or click</div>
+        )}
+      </div>
     </div>
   );
 };
